aeac43dbb5c5650d1b3a300cf8763c0a6ce47036
yu_heejin <41100813+yuheejin@users.noreply.github.com>
2023-08-04 08:41:01 +0900

    Update days16.Ex05.java update

--- Education/Java/days16/Ex05.java
+++ Education/Java/days16/Ex05.java
@@ -3,7 +3,7 @@ package Education.Java.days16;
 public class Ex05 {
 	public static void main(String[] args) {
 		// this : 클래스 자기 자신의 주소값을 갖는 참조변수
-		// super : 부모의 주소값을 갑는 참조변수
+		// super : 부모의 주소값을 갖는 참조변수
 		// 			  자식클래스에서 부모클래스의 멤버를 참조하는 참조변수
 		// 예) Regular -> SalesMan
 		//					  super.gerPay() + this.sales*this.comm;
